f0ca2403b1e2d065606770940ce4a487298964f6
Piratux <58703216+Piratux@users.noreply.github.com>
2024-02-08 21:24:15 +0200

    update queue_bruteforce.sh for cluster

--- cluster-version/queue_bruteforce.sh
+++ cluster-version/queue_bruteforce.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-#SBATCH -p main
-#SBATCH -n8
+#SBATCH --partition=main
+#SBATCH --nodes=20
+#SBATCH --ntasks-per-node=48
+#SBATCH --time=0:30:00
 module load openmpi
 mpiCC -std=c++17 -O2 -o bruteforce-cluster bruteforce-cluster.cpp
-mpirun bruteforce-cluster
+
+# -n should match --nodes * --ntasks-per-node (as this will indicate how many processes to spawn)
+mpirun -n 960 bruteforce-cluster
